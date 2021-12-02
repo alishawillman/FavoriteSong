471c97d9c859cffabc441c97813007bd4343bee8
alishawillman <72734094+alishawillman@users.noreply.github.com>
2021-12-02 02:44:45 -0700

    Add project files.

---
+++ FavoriteSong.com/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+var string = "Wonderwall by Oasis";
+var myAnimation = string.split("");
+var el = document.getElementById('myAnimation');
+(function animate() {
+    myAnimation.length > 0 ? el.innerHTML +=
+        myAnimation.shift() : clearTimeout(running);
+    var running = setTimeout(animate, 90);
+})();
